0afe96fed30674019dee5204c4646d9dd61905d5
Erin <erinzylstra@gmail.com>
2021-04-24 15:38:53 -0700

    Added a multistate model with random site effects

--- Multistate.R
+++ Multistate.R
@@ -151,7 +151,7 @@ disttocity <- read.csv('PlotDistToCity.csv',header=TRUE,stringsAsFactors=FALSE)
   }
 
 #-----------------------------------------------------------------------------------------------# 
-# Run simple CJS model in JAGS, no covariates, no random effects
+# Run simple multistate model in JAGS, no covariates, no random effects
 #-----------------------------------------------------------------------------------------------# 
 #Prep data objects for JAGS (no covariates, no random effects)
   tortdata <- list(y=as.matrix(cr.mat),
@@ -237,7 +237,8 @@ disttocity <- read.csv('PlotDistToCity.csv',header=TRUE,stringsAsFactors=FALSE)
   sink()
 
 #MCMC settings, parameters, initial values  
-  ni <- 500; na <- 500; nb <- 500; nc <- 3; ni.tot <- ni + nb
+  ni <- 20000; na <- 2000; nb <- 10000; nc <- 3; ni.tot <- ni + nb
+  # ni <- 500; na <- 500; nb <- 500; nc <- 3; ni.tot <- ni + nb
 	
 	params <- c('alpha.p1','alpha.p2','beta.phi1','beta.phi2','gamma.psi',
 	            'p1.mn','p2.mn','phi1.mn','phi2.mn','psi12.mn')
@@ -255,3 +256,391 @@ disttocity <- read.csv('PlotDistToCity.csv',header=TRUE,stringsAsFactors=FALSE)
                   n.chains=nc, n.adapt=na, n.iter=ni.tot, n.burnin=nb,
                   parallel=T, n.cores=3, DIC=FALSE)  
 	print(fit.ms0)
+
+#-----------------------------------------------------------------------------------------------# 
+# Run multistate model in JAGS, with covariates (fixed effects), no random effects
+#-----------------------------------------------------------------------------------------------#
+#Formatting individual covariates
+	sex <- cr$sex
+	sex[sex==3] <- NA
+	male.ind <- sex-1
+	
+#Formatting site covariate: distance to nearest "major" city (population > 10,000)
+	names(disttocity)[names(disttocity)=='code'] <- 'plot'
+	disttocity <- disttocity[order(disttocity$plot),]
+	# #Check that plots are in the same order they're found in capture histories
+	# all.equal(unique(cr$plot),unique(disttocity$plot))
+	dist.mn <- mean(disttocity$dist.km)
+	dist.sd <- sd(disttocity$dist.km)
+	distance <- (disttocity$dist.km - dist.mn)/dist.sd
+	
+#Formatting site covariate: precipitation normals
+	precip.norms <- precip.norms[order(precip.norms$plot),]
+	# #Check that plots are in the same order they're found in capture histories
+	# all.equal(unique(cr$plot),unique(precip.norms$plot))
+	precipnorm.mn <- mean(precip.norms$ppt.mm)
+	precipnorm.sd <- sd(precip.norms$ppt.mm)
+	precip.norm <- (precip.norms$ppt.mm - precipnorm.mn)/precipnorm.sd
+	
+#Formatting site*year covariate: drought
+  #Going to calculate mean PDSI averaged over the previous 12 and 24 months (used 24-mon index in 2013 paper)
+	pdsi <- pdsi[with(pdsi,order(div,yr,mon)),]
+	pdsi$pdsi.12 <- NA
+	pdsi$pdsi.24 <- NA
+	jul.index <- which(pdsi$yr %in% 1988:2020 & pdsi$mon==7)
+	for(i in jul.index){
+	  pdsi$pdsi.12[i] <- mean(pdsi$pdsi[(i-11):i])
+	  pdsi$pdsi.24[i] <- mean(pdsi$pdsi[(i-23):i])
+	}
+	pdsi.mn <- pdsi[!is.na(pdsi$pdsi.12),c('div','yr','pdsi.12','pdsi.24')]
+  #Link plot to the appropriate climate division
+	plotdiv <- data.frame(plot=unique(cr$plot),div=c(1,1,3,1,6,6,6,1,3,6,3,6,5,6,6,3,7),stringsAsFactors=FALSE)
+	pdsi.plot <- expand.grid(yr=1988:2020,plot=plotdiv$plot)
+	pdsi.plot$div <- plotdiv$div[match(pdsi.plot$plot,plotdiv$plot)]
+	pdsi.plot <- join(pdsi.plot,pdsi.mn,by=c('div','yr'),type='left')
+	pdsi.12 <- dcast(pdsi.plot,plot~yr,mean,value.var='pdsi.12')
+	pdsi.24 <- dcast(pdsi.plot,plot~yr,mean,value.var='pdsi.24')
+	# #Check that plots are in the same order they're found in capture histories
+	# all.equal(unique(cr$plot),as.character(pdsi.12$plot))	
+	# all.equal(unique(cr$plot),as.character(pdsi.24$plot))	
+	pdsi.12.mat <- as.matrix(pdsi.12[,2:ncol(pdsi.12)])
+	pdsi.24.mat <- as.matrix(pdsi.24[,2:ncol(pdsi.24)])
+	pdsi12.mn <- mean(pdsi.12.mat)
+	pdsi12.sd <- sd(pdsi.12.mat)
+	pdsi24.mn <- mean(pdsi.24.mat)
+	pdsi24.sd <- sd(pdsi.24.mat)	
+	pdsi12.z <- (pdsi.12.mat - pdsi12.mn)/pdsi12.sd
+	pdsi24.z <- (pdsi.24.mat - pdsi24.mn)/pdsi24.sd
+
+#Formatting site*year covariate: precipitation	
+	precip <- precip[with(precip,order(plot,season)),]
+	# #Check that plots are in the same order they're found in capture histories
+	# all.equal(unique(cr$plot),unique(precip$plot))
+	#Cumulative precipitation (mm) at each plot from Aug-Jul
+	precip.aj <- ddply(precip[precip$season %in% 1988:2020,],.(plot,season),summarize,ppt=sum(ppt))
+	#Standardizing values by plot mean/SDs (can then compare effects of precipitation that's 1-SD above average)
+	precipbyplot <- ddply(precip.aj,.(plot),summarize,ppt.mn=mean(ppt),ppt.sd=sd(ppt))
+	precip.aj <- join(precip.aj,precipbyplot,by='plot',type='left')
+	precip.aj$ppt.z <- (precip.aj$ppt - precip.aj$ppt.mn)/precip.aj$ppt.sd
+	# #check:
+	# ddply(precip.aj,.(plot),summarize,mn=mean(ppt.z),sd=sd(ppt.z))
+	ppt.df <- dcast(precip.aj,plot~season,value.var='ppt.z')
+	ppt.mat <- as.matrix(ppt.df[,2:ncol(ppt.df)])
+	
+#Plot index for each tortoise
+	plots <- data.frame(plot=unique(cr$plot),plot.index=1:length(unique(cr$plot)))
+  plot.index <- plots$plot.index[match(cr$plot,plots$plot)]	
+
+#Prep data objects for JAGS
+  ntorts <- nrow(cr.mat)             #number of tortoises
+  nyears <- ncol(cr.mat)             #number of occasions
+  nplots <- length(unique(cr$plot))  #number of plots	
+  
+  tortdata <- list(y=as.matrix(cr.mat),
+                   ntorts=ntorts,
+                   nyears=nyears,
+                   first=first,
+                   male=male.ind,
+                   plot=plot.index,
+                   distance=distance,
+                   mean.precip=precip.norm,
+                   drought=pdsi24.z,
+                   precip=ppt.mat)
+
+#JAGS model: covariates on adult and juvenile parameters (assuming constant transition rates)	
+  sink("MS_CovarsAdultJuv.txt")
+  cat("
+    model{
+
+      #-- Priors and constraints
+
+      alpha.p1 ~ dlogis(0,1)
+      alpha.p2 ~ dlogis(0,1)
+      beta.phi1 ~ dlogis(0,1)
+      beta.phi2 ~ dlogis(0,1)
+      gamma.psi ~ dlogis(0,1)
+
+      a1.precip ~ dnorm(0,0.1)
+      a2.male ~ dnorm(0,0.1)
+      a2.precip ~ dnorm(0,0.1)
+      b1.mnprecip ~ dnorm(0,0.1)
+      b2.male ~ dnorm(0,0.1)
+      b2.distance ~ dnorm(0,0.1)
+      b2.mnprecip ~ dnorm(0,0.1)
+      b2.drought ~ dnorm(0,0.1)
+      b2.int ~ dnorm(0,0.1)
+      psi.male ~ dunif(0,1)
+
+      for (i in 1:ntorts){
+        for(t in first[i]:(nyears-1)){
+
+          logit(p1[i,t]) <- alpha.p1 + a1.precip*precip[plot[i],t]
+          logit(phi1[i,t]) <- beta.phi1 + b1.mnprecip*mean.precip[plot[i]]
+
+          logit(psi12[i,t]) <- gamma.psi
+
+          logit(p2[i,t]) <- alpha.p2 + a2.male*male[i] + a2.precip*precip[plot[i],t]
+          logit(phi2[i,t]) <- beta.phi2 + b2.male*male[i] + b2.distance*distance[plot[i]] +
+                              b2.mnprecip*mean.precip[plot[i]] + b2.drought*drought[plot[i],t] +
+                              b2.int*mean.precip[plot[i]]*drought[plot[i],t]
+
+          #Define state transition probabilities
+          #First index = states at time t-1, last index = states at time t
+          ps[1,i,t,1] <- phi1[i,t] * (1-psi12[i,t])
+          ps[1,i,t,2] <- phi1[i,t] * psi12[i,t]
+          ps[1,i,t,3] <- 1-phi1[i,t]
+          ps[2,i,t,1] <- 0
+          ps[2,i,t,2] <- phi2[i,t]
+          ps[2,i,t,3] <- 1-phi2[i,t]
+          ps[3,i,t,1] <- 0
+          ps[3,i,t,2] <- 0
+          ps[3,i,t,3] <- 1
+
+          #Define stage-dependent detection probabilities
+          #First index = states at time t, last index = detection type at time t
+          po[1,i,t,1] <- p1[i,t]
+          po[1,i,t,2] <- 0
+          po[1,i,t,3] <- 1-p1[i,t]
+          po[2,i,t,1] <- 0
+          po[2,i,t,2] <- p2[i,t]
+          po[2,i,t,3] <- 1-p2[i,t]
+          po[3,i,t,1] <- 0
+          po[3,i,t,2] <- 0
+          po[3,i,t,3] <- 1
+
+        } #t
+      } #i
+
+      #-- Likelihood
+
+      for(i in 1:ntorts){
+        z[i,first[i]] <- y[i,first[i]]
+        male[i] ~ dbern(psi.male)
+
+        for (t in (first[i]+1):nyears){
+
+          #State process: draw State[t] given State[t-1]
+          z[i,t] ~ dcat(ps[z[i,t-1],i,t-1,])
+
+          #Observation process: draw Obs[t] given State[t]
+          y[i,t] ~ dcat(po[z[i,t],i,t-1,])
+
+        } #t
+      } #i
+
+      #-- Derived parameters
+
+      logit(psi12.mn) <- gamma.psi
+      logit(phi1.mn) <- beta.phi1
+      logit(p1.mn) <- alpha.p1
+
+      phi2.f <- exp(beta.phi2)/(1 + exp(beta.phi2))
+      phi2.m <- exp(beta.phi2 + b2.male)/(1 + exp(beta.phi2 + b2.male))
+      p2.f <- exp(alpha.p2)/(1 + exp(alpha.p2))
+      p2.m <- exp(alpha.p2 + a2.male)/(1 + exp(alpha.p2 + a2.male))
+
+    } #model
+  ",fill=TRUE)
+  sink()
+  
+#MCMC settings, parameters, initial values  
+  ni <- 20000; na <- 2000; nb <- 10000; nc <- 3; ni.tot <- ni + nb
+  # ni <- 500; na <- 500; nb <- 500; nc <- 3; ni.tot <- ni + nb
+	
+	params <- c('alpha.p1','a1.precip',
+	            'beta.phi1','b1.mnprecip',
+	            'gamma.psi',
+	            'alpha.p2','a2.male','a2.precip',
+	            'beta.phi2','b2.male','b2.distance','b2.mnprecip','b2.drought','b2.int',
+	            'psi.male',
+	            'p1.mn','phi1.mn','psi12.mn','phi2.f','phi2.m','p2.f','p2.m')
+
+  inits <- function() {list(alpha.p1=runif(1,-1,1),
+                            alpha.p2=runif(1,-1,2),
+                            beta.phi1=runif(1,-1,1),
+                            beta.phi2=runif(1,1,3),
+                            gamma.psi=runif(1,-2,0),
+                            a1.precip=runif(1,-0.5,0.5),
+                            a2.male=runif(1,-0.5,0.5),
+                            a2.precip=runif(1,-0.5,0.5),
+                            b1.mnprecip=runif(1,-0.5,0.5),
+                            b2.male=runif(1,-0.5,0.5),
+                            b2.distance=runif(1,-0.5,0.5),
+                            b2.mnprecip=runif(1,-0.5,0.5),
+                            b2.drought=runif(1,-0.5,0.5),
+                            b2.int=runif(1,-0.5,0.5),
+                            psi.male=runif(1,0,1),
+                            male=ifelse(is.na(male.ind),1,NA),
+                            z=ch.init(as.matrix(cr.mat),first1,first2))}
+  
+#Run model
+	fit.ms1 <- jags(data=tortdata, inits=inits, parameters.to.save=params,
+                  model.file='MS_CovarsAdultJuv.txt',
+                  n.chains=nc, n.adapt=na, n.iter=ni.tot, n.burnin=nb,
+                  parallel=T, n.cores=3, DIC=FALSE)  
+	print(fit.ms1)  
+	
+#-----------------------------------------------------------------------------------------------# 
+# Run multistate model in JAGS, with covariates (fixed effects) and random site effects
+#-----------------------------------------------------------------------------------------------#	
+#Prep data objects for JAGS
+  ntorts <- nrow(cr.mat)             #number of tortoises
+  nyears <- ncol(cr.mat)             #number of occasions
+  nplots <- length(unique(cr$plot))  #number of plots
+  
+  tortdata <- list(y=as.matrix(cr.mat),
+                   ntorts=ntorts,
+                   nyears=nyears,
+                   nplots=nplots,
+                   first=first,
+                   male=male.ind,
+                   plot=plot.index,
+                   distance=distance,
+                   mean.precip=precip.norm,
+                   drought=pdsi24.z,
+                   precip=ppt.mat)
+
+#JAGS model: covariates on adult and juvenile parameters, random site effects	
+  sink("MS_CovarsAdultJuv_siteREs.txt")
+  cat("
+    model{
+
+      #-- Priors and constraints
+
+      alpha.p1 ~ dlogis(0,1)
+      alpha.p2 ~ dlogis(0,1)
+      beta.phi1 ~ dlogis(0,1)
+      beta.phi2 ~ dlogis(0,1)
+      gamma.psi ~ dlogis(0,1)
+
+      a1.precip ~ dnorm(0,0.1)
+      a2.male ~ dnorm(0,0.1)
+      a2.precip ~ dnorm(0,0.1)
+      b1.mnprecip ~ dnorm(0,0.1)
+      b2.male ~ dnorm(0,0.1)
+      b2.distance ~ dnorm(0,0.1)
+      b2.mnprecip ~ dnorm(0,0.1)
+      b2.drought ~ dnorm(0,0.1)
+      b2.int ~ dnorm(0,0.1)
+      psi.male ~ dunif(0,1)
+
+      sigma.site ~ dt(0,pow(2.5,-2),1)T(0,)  #Half-cauchy prior
+      tau.site <- 1/(sigma.site*sigma.site)
+
+      for(p in 1:nplots){
+        e.site[p] ~ dnorm(0,tau.site)
+      }
+
+      for (i in 1:ntorts){
+        for(t in first[i]:(nyears-1)){
+
+          logit(p1[i,t]) <- alpha.p1 + a1.precip*precip[plot[i],t]
+          logit(phi1[i,t]) <- beta.phi1 + b1.mnprecip*mean.precip[plot[i]]
+
+          logit(psi12[i,t]) <- gamma.psi
+
+          logit(p2[i,t]) <- alpha.p2 + a2.male*male[i] + a2.precip*precip[plot[i],t]
+          logit(phi2[i,t]) <- beta.phi2 + b2.male*male[i] + b2.distance*distance[plot[i]] +
+                              b2.mnprecip*mean.precip[plot[i]] + b2.drought*drought[plot[i],t] +
+                              b2.int*mean.precip[plot[i]]*drought[plot[i],t] + e.site[plot[i]]
+
+          #Define state transition probabilities
+          #First index = states at time t-1, last index = states at time t
+          ps[1,i,t,1] <- phi1[i,t] * (1-psi12[i,t])
+          ps[1,i,t,2] <- phi1[i,t] * psi12[i,t]
+          ps[1,i,t,3] <- 1-phi1[i,t]
+          ps[2,i,t,1] <- 0
+          ps[2,i,t,2] <- phi2[i,t]
+          ps[2,i,t,3] <- 1-phi2[i,t]
+          ps[3,i,t,1] <- 0
+          ps[3,i,t,2] <- 0
+          ps[3,i,t,3] <- 1
+
+          #Define stage-dependent detection probabilities
+          #First index = states at time t, last index = detection type at time t
+          po[1,i,t,1] <- p1[i,t]
+          po[1,i,t,2] <- 0
+          po[1,i,t,3] <- 1-p1[i,t]
+          po[2,i,t,1] <- 0
+          po[2,i,t,2] <- p2[i,t]
+          po[2,i,t,3] <- 1-p2[i,t]
+          po[3,i,t,1] <- 0
+          po[3,i,t,2] <- 0
+          po[3,i,t,3] <- 1
+
+        } #t
+      } #i
+
+      #-- Likelihood
+
+      for(i in 1:ntorts){
+        z[i,first[i]] <- y[i,first[i]]
+        male[i] ~ dbern(psi.male)
+
+        for (t in (first[i]+1):nyears){
+
+          #State process: draw State[t] given State[t-1]
+          z[i,t] ~ dcat(ps[z[i,t-1],i,t-1,])
+
+          #Observation process: draw Obs[t] given State[t]
+          y[i,t] ~ dcat(po[z[i,t],i,t-1,])
+
+        } #t
+      } #i
+
+      #-- Derived parameters
+
+      logit(psi12.mn) <- gamma.psi
+      logit(phi1.mn) <- beta.phi1
+      logit(p1.mn) <- alpha.p1
+
+      phi2.f <- exp(beta.phi2)/(1 + exp(beta.phi2))
+      phi2.m <- exp(beta.phi2 + b2.male)/(1 + exp(beta.phi2 + b2.male))
+      p2.f <- exp(alpha.p2)/(1 + exp(alpha.p2))
+      p2.m <- exp(alpha.p2 + a2.male)/(1 + exp(alpha.p2 + a2.male))
+
+    } #model
+  ",fill=TRUE)
+  sink()
+
+#MCMC settings, parameters, initial values  
+  ni <- 20000; na <- 2000; nb <- 10000; nc <- 3; ni.tot <- ni + nb
+  # ni <- 500; na <- 500; nb <- 500; nc <- 3; ni.tot <- ni + nb
+
+	params <- c('alpha.p1','a1.precip',
+	            'beta.phi1','b1.mnprecip',
+	            'gamma.psi',
+	            'alpha.p2','a2.male','a2.precip',
+	            'beta.phi2','b2.male','b2.distance','b2.mnprecip','b2.drought','b2.int',
+	            'psi.male','sigma.site','e.site',
+	            'p1.mn','phi1.mn','psi12.mn',
+	            'phi2.f','phi2.m','p2.f','p2.m')
+
+  inits <- function() {list(alpha.p1=runif(1,-1,1),
+                            alpha.p2=runif(1,-1,2),
+                            beta.phi1=runif(1,-1,1),
+                            beta.phi2=runif(1,1,3),
+                            gamma.psi=runif(1,-2,0),
+                            a1.precip=runif(1,-0.5,0.5),
+                            a2.male=runif(1,-0.5,0.5),
+                            a2.precip=runif(1,-0.5,0.5),
+                            b1.mnprecip=runif(1,-0.5,0.5),
+                            b2.male=runif(1,-0.5,0.5),
+                            b2.distance=runif(1,-0.5,0.5),
+                            b2.mnprecip=runif(1,-0.5,0.5),
+                            b2.drought=runif(1,-0.5,0.5),
+                            b2.int=runif(1,-0.5,0.5),
+                            psi.male=runif(1,0,1),
+                            sigma.site=runif(1,0,3),
+                            male=ifelse(is.na(male.ind),1,NA),
+                            z=ch.init(as.matrix(cr.mat),first1,first2))}
+
+#Run model
+	fit.ms2 <- jags(data=tortdata, inits=inits, parameters.to.save=params,
+                  model.file='MS_CovarsAdultJuv_siteREs.txt',
+                  n.chains=nc, n.adapt=na, n.iter=ni.tot, n.burnin=nb,
+                  parallel=T, n.cores=3, DIC=FALSE)  
+	print(fit.ms2)  
+	
+	
+	
